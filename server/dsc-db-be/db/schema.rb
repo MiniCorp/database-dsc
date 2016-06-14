e08dbe1b170f75eb7793973db45c7716cadc1af6
Steve Thornton <ste.thornton@gmail.com>
2016-06-14 10:09:48 +0100

    Minor tweaks

--- server/dsc-db-be/db/schema.rb
+++ server/dsc-db-be/db/schema.rb
@@ -15,6 +15,34 @@ ActiveRecord::Schema.define(version: 20160527111710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "btree_gin"
+  enable_extension "btree_gist"
+  enable_extension "citext"
+  enable_extension "cube"
+  enable_extension "dblink"
+  enable_extension "dict_int"
+  enable_extension "dict_xsyn"
+  enable_extension "earthdistance"
+  enable_extension "fuzzystrmatch"
+  enable_extension "hstore"
+  enable_extension "intarray"
+  enable_extension "ltree"
+  enable_extension "pg_stat_statements"
+  enable_extension "pg_trgm"
+  enable_extension "pgcrypto"
+  enable_extension "pgrowlocks"
+  enable_extension "pgstattuple"
+  enable_extension "sslinfo"
+  enable_extension "tablefunc"
+  enable_extension "unaccent"
+  enable_extension "uuid-ossp"
+  enable_extension "xml2"
+
+  create_table "ar_internal_metadata", primary_key: "key", force: :cascade do |t|
+    t.string   "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "authorizations", force: :cascade do |t|
     t.string   "provider"
@@ -29,8 +57,8 @@ ActiveRecord::Schema.define(version: 20160527111710) do
   add_index "authorizations", ["user_id"], name: "index_authorizations_on_user_id", using: :btree
 
   create_table "companies", force: :cascade do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
     t.string   "name"
     t.string   "logo"
     t.text     "short_description"
@@ -62,7 +90,7 @@ ActiveRecord::Schema.define(version: 20160527111710) do
     t.text     "custom_field_3"
     t.text     "custom_field_4"
     t.jsonb    "office_locations",      default: {}
-    t.string   "tags",                  default: [],    array: true
+    t.string   "tags",                  default: [],                 array: true
     t.jsonb    "founders"
     t.string   "acquired"
     t.integer  "revenue"
@@ -80,9 +108,9 @@ ActiveRecord::Schema.define(version: 20160527111710) do
     t.string   "logo"
     t.text     "short_description"
     t.text     "programs"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.text     "hub_type",             default: [], array: true
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
+    t.text     "hub_type",             default: [],              array: true
     t.date     "application_deadline"
     t.text     "long_description"
     t.string   "founded"
@@ -90,7 +118,7 @@ ActiveRecord::Schema.define(version: 20160527111710) do
     t.string   "contact_detail"
     t.text     "address"
     t.jsonb    "contact_urls",         default: []
-    t.text     "events",               default: [], array: true
+    t.text     "events",               default: [],              array: true
     t.jsonb    "alumni",               default: []
     t.datetime "deleted_at"
     t.text     "custom_field_1"
@@ -100,7 +128,7 @@ ActiveRecord::Schema.define(version: 20160527111710) do
     t.string   "website"
     t.text     "video_url"
     t.jsonb    "social_accounts"
-    t.string   "tags",                 default: [], array: true
+    t.string   "tags",                 default: [],              array: true
     t.boolean  "funding_provided"
     t.float    "lat"
     t.float    "lng"
@@ -119,10 +147,10 @@ ActiveRecord::Schema.define(version: 20160527111710) do
     t.jsonb    "founders",              default: {}
     t.text     "short_description"
     t.string   "local_office"
-    t.text     "tags",                  default: [], array: true
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.text     "funding_types",         default: [], array: true
+    t.text     "tags",                  default: [],              array: true
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
+    t.text     "funding_types",         default: [],              array: true
     t.integer  "investment_size"
     t.string   "funds_raised"
     t.text     "regions"
@@ -161,12 +189,12 @@ ActiveRecord::Schema.define(version: 20160527111710) do
     t.text     "short_description"
     t.string   "headquarters"
     t.string   "local_office"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                  null: false
+    t.datetime "updated_at",                                  null: false
     t.boolean  "emea_hq",                     default: false
     t.integer  "employees"
     t.boolean  "events_space",                default: false
-    t.text     "functions",                   default: [],    array: true
+    t.text     "functions",                   default: [],                 array: true
     t.text     "long_description"
     t.text     "events_space_qualifiers"
     t.string   "next_event"
@@ -179,7 +207,7 @@ ActiveRecord::Schema.define(version: 20160527111710) do
     t.text     "custom_field_4"
     t.jsonb    "startup_packages"
     t.text     "video_url"
-    t.string   "tags",                        default: [],    array: true
+    t.string   "tags",                        default: [],                 array: true
     t.float    "lat"
     t.float    "lng"
     t.boolean  "building_product_in_ireland", default: false
@@ -191,8 +219,8 @@ ActiveRecord::Schema.define(version: 20160527111710) do
 
   create_table "pg_search_documents", force: :cascade do |t|
     t.text     "content"
-    t.integer  "searchable_id"
     t.string   "searchable_type"
+    t.integer  "searchable_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
@@ -216,8 +244,8 @@ ActiveRecord::Schema.define(version: 20160527111710) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "password_digest"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.integer  "user_type",       default: 0
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
