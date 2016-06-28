6d904bd9c6efec7044effd4655e773cbcea505e6
Steve Thornton <ste.thornton@gmail.com>
2016-06-28 16:31:25 +0100

    Fix backend tests

--- server/dsc-db-be/spec/factories/company.rb
+++ server/dsc-db-be/spec/factories/company.rb
@@ -3,5 +3,6 @@ FactoryGirl.define do
     name { Faker::Company.name }
     logo "https://placeholdit.imgix.net/~text?txtsize=33&txt=Company%20Logo&w=250&h=140"
     short_description { Faker::Company.catch_phrase }
+    is_live true
   end
 end
