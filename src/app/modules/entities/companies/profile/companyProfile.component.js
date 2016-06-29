a142da5fecddc218a0fde03941d80ae31c92626b
Brian Kenny <brian@minicorp.ie>
2016-06-29 11:01:58 +0100

    Add rounding of employees

--- src/app/modules/entities/companies/profile/companyProfile.component.js
+++ src/app/modules/entities/companies/profile/companyProfile.component.js
@@ -27,8 +27,40 @@
         this.map.panTo(markerPosition);
       };
 
+
+
       this.company = getCompanyService.find($stateParams.id).then(function(company) {
         controller.company = company;
+
+
+        switch (true) {
+          case (company.employees > 1 && company.employees < 6):
+            controller.company.employeeRangeText = "1-5";
+            break;
+          case (company.employees > 5 && company.employees < 11):
+            controller.company.employeeRangeText = "6-10";
+            break;
+          case (company.employees > 10 && company.employees < 26):
+            controller.company.employeeRangeText = "11-25";
+            break;
+          case (company.employees > 25 && company.employees < 51):
+            controller.company.employeeRangeText = "26-50";
+            break;
+          case (company.employees > 49 && company.employees < 101):
+            controller.company.employeeRangeText = "50-100";
+            break;
+          case (company.employees > 100 && company.employees < 251):
+            controller.company.employeeRangeText = "100-250";
+            break;
+          case (company.employees > 249 && company.employees < 501):
+            controller.company.employeeRangeText = "250-500";
+            break;
+          case (company.employees > 499):
+            controller.company.employeeRangeText = "> 500";
+            break;
+          default:
+            break;
+        }
       });
     });
 
