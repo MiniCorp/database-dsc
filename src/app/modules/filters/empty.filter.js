01c8e74c29488311c72051f2353becbff52b0615
Brian Kenny <brian@minicorp.ie>
2016-07-25 21:52:39 +0100

    Global markets fix

--- src/app/modules/filters/empty.filter.js
+++ src/app/modules/filters/empty.filter.js
@@ -5,10 +5,16 @@
     .module('dscFe')
     .filter('emptyFilter', function() {
 
+      function hasWhiteSpace(s) {
+        return /\s/g.test(s);
+      }
+
       return function(array) {
         var filteredArray = [];
           angular.forEach(array, function(item) {
-            if (item) filteredArray.push(item);
+            if (item && !hasWhiteSpace(item)) {
+              filteredArray.push(item);
+            }
           });
         return filteredArray;
       };
