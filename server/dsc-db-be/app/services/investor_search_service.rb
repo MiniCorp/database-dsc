8d4dc8adf71740d37fecd53bfe3a32f8b2b7b18c
Brian Kenny <brian@minicorp.ie>
2016-09-21 16:52:44 +0100

    Fix issue with search on investors

--- server/dsc-db-be/app/services/investor_search_service.rb
+++ server/dsc-db-be/app/services/investor_search_service.rb
@@ -6,15 +6,20 @@ class InvestorSearchService
 
   def call
     if @params[:searchText].present?
-      investors = Investor.live(true).search(@params[:searchText])
+      investors = Investor.search(@params[:searchText])
     elsif @params[:tag].present?
       investors = Investor.live(true).search_by_tag(@params[:tag])
     else
       investors = Investor.live(true).all
     end
 
-    investors = investors.funding_types(@params[:fundingTypes]) if @params[:fundingTypes].present?
-    investors = investors.select_numeric_scope('investment_size', @params[:investmentSize]) if @params[:investmentSize]
+    if @params[:fundingTypes] && !@params[:fundingTypes].include?('false')
+      investors = investors.funding_types(@params[:fundingTypes])
+    end
+    if @params[:investmentSize] && @params[:investmentSize] != '100000000'
+      investors = investors.select_numeric_scope('investment_size', @params[:investmentSize])
+    end
+
     investors = investors.deal_structure(@params[:dealStructure]) if @params[:dealStructure].present?
 
     investors
